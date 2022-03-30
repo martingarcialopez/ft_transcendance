ebadd2b61d072cf5aef93bb710fcd5410ad8d8c9
Martín <mgarcia-@student.42.fr>
2022-03-30 12:29:35 +0200

    remove .git inherent to nest project

--- back-end/src/app.service.ts
+++ back-end/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'yoyoyoyo que fue lo que paso !';
+    return 'yoyoyoyo que fue lo que paso!';
   }
 }
