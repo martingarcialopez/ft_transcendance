c09d2b15e3f4bbb1b251b8b4b40d52badc034c8d
Raphack <amamars201@protonmail.com>
2022-04-11 17:16:56 +0200

    url normaly OK

--- front-end/src/auth/LoginButton.js
+++ front-end/src/auth/LoginButton.js
@@ -13,7 +13,7 @@ const LoginButton = () => {
       `https://${domain}?` + 
       `client_id=${client_id}&` + 
       `redirect_uri=${redirect_uri}&` +
-      `response_type=${response_type}` +
+      `response_type=${response_type}&` +
       `scope=${scope}&` +
       `state=${state}&`, {
         redirect: "manual"
