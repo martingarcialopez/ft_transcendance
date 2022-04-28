366fbe4ee0fc420fd53de453b59012ec5b367799
raphou <raphou1@MacBook-Pro.local>
2022-04-28 03:11:48 -0700

    Nomenclature site OK // Connexion without 42 OK // Responsive OK

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './components/Home'
-import { Navbar } from './components/Navbar'
+//import { Navbar } from './components/Navbar'
+import { ResponsiveAppBar } from './components/NavBar2'
 import { NoMatch } from './components/NoMatch'
 import { Users } from './components/Users'
 import { UserDetails } from './components/UserDetails'
 import { Admin } from './components/Admin'
 import { AuthProvider } from './components/auth'
-import { Login } from './components/Login'
+//import { Login } from './components/Login'
 import { Profile } from './components/Profile'
 import { RequireAuth } from './components/RequireAuth'
 import { Chat } from './components/Chat'
 import { Pong } from './components/Pong'
 import { Challenges } from './components/Challenges'
+import SignIn from './components/SignIn2'
+import SignUp from './components/SignUp'
 
 function App() {
   return (
     <AuthProvider>
-      <Navbar />
+      <ResponsiveAppBar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/login' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='/chat' element={<Chat />} />
         <Route path="/challenges" element={<Challenges />} />
         <Route
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,10 +1,8 @@
 import LoginButton from "../auth/LoginButton";
 import LogoutButton from "../auth/LogoutButton";
-import { useNavigate } from 'react-router-dom'
 import { useAuth } from './auth'
 
 export const Home = () => {
-  const navigate = useNavigate()
   const auth = useAuth()
 
   return (
