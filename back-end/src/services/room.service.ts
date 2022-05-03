099da7b9863b6648b294b9af3b981f77122bdf62
xibaochat <wmmxxbb@gmail.com>
2022-05-03 13:19:51 +0200

    Add password hashing on room creation

--- back-end/src/services/room.service.ts
+++ back-end/src/services/room.service.ts
@@ -8,6 +8,7 @@ import { Participant } from '../models/participant.entity';
 import { User } from '../models/user.entity';
 import { classToPlain, Exclude } from 'class-transformer';
 import { plainToClass } from 'class-transformer';
+import * as bcrypt from 'bcrypt';
 
 
 @Injectable()
@@ -18,30 +19,36 @@ export class RoomService {
         private readonly roomRepository: Repository<Room>,
     ){}
 
-/*
-** Create a new channel(room)
-** :param(RoomDto) the infos of the channel given by front
-** :return (RoomSnippetDto) dto contains new channel id and its name
-*/	async createRoom(roomDto: RoomDto): Promise<RoomSnippetDto>
+	/*
+	** Create a new channel(room)
+	** :param(RoomDto) the infos of the channel given by front
+	** :return (RoomSnippetDto) dto contains new channel id and its name
+	*/	async createRoom(roomDto: RoomDto): Promise<RoomSnippetDto>
 	{
         const new_room = new Room();
 		new_room.name = roomDto.name;
 		new_room.typeChannel = roomDto.typeChannel;
-		new_room.password = roomDto.password;
+
+		const password = roomDto.password;
+		const saltOrRounds = 10;
+		const hash = await bcrypt.hash(password, saltOrRounds);
+		console.log('here ', hash);
+
+		new_room.password = hash;
 		new_room.owner = roomDto.owner;
 		//		new_room.members = roomDto.members;
 		await this.roomRepository.save(new_room);
 		const dto = plainToClass(RoomSnippetDto, new_room);
 		console.log(typeof 'HERE', typeof dto, dto);
 		return dto;
-    }
+	}
 
 
-/*
-** Get all user in the given id room
-** :Param(room_Id: number)
-** return (an obj contains roomid, roomName, and all participants)
-*/
+	/*
+	** Get all user in the given id room
+	** :Param(room_Id: number)
+	** return (an obj contains roomid, roomName, and all participants)
+	*/
 	async getRoom(room_Id: number): Promise<Room>
 	{
 		const room = await this.roomRepository.findOne(room_Id);
@@ -55,11 +62,11 @@ export class RoomService {
 		return p;
 	}
 
-/*
-** Delete the whole column corresponding to the id
-** :param (id:number) id is the primary key in room table
-** :return void
-*/
+	/*
+	** Delete the whole column corresponding to the id
+	** :param (id:number) id is the primary key in room table
+	** :return void
+	*/
 	async deleteRoom(id: number): Promise<void> {
         await this.roomRepository.delete(id);
     }
