528a2e8999154532370561078ecbe394e7c4b030
maxime-42 <mkayumba@student.42.fr>
2022-05-04 19:52:34 +0200

    ajout du fichier roomReducer.tsx avant la fusion avec dev

--- front-end/App/src/redux/reducers/roomReducer.tsx
+++ front-end/App/src/redux/reducers/roomReducer.tsx
@@ -4,7 +4,7 @@ import { E_ActionType } from "../../type/Enum";
 let arrayRoom: T_Room[] = [
   {
     name: "LIVERPOOL",
-    id: 1,
+    id: 1, //this id i creat myself
     typeRoom: "public",
     password: "",
     owner: [],
