2f2f8a3726fdedff0d37b609f68a2fee3ad03649
Martín <mgarcia-@student.42.fr>
2022-05-05 23:59:34 +0200

    uncomment docker-compose + recover CreateUser.dto

--- back-end/src/dtos/in/CreateUser.dto.ts
+++ back-end/src/dtos/in/CreateUser.dto.ts
@@ -1,6 +1,24 @@
+import { IsString, IsNotEmpty, IsOptional } from 'class-validator';
+
 export class CreateUserDto {
-  firstname: string;
-  lastname: string;
-  username: string;
-  password: string;
-}
+
+    @IsNotEmpty()
+    @IsString()
+    firstname: string;
+
+    @IsNotEmpty()
+    @IsString()
+    lastname: string;
+
+    @IsNotEmpty()
+    @IsString()
+    username: string;
+
+    @IsNotEmpty()
+    @IsString()
+    password: string;
+
+    @IsOptional()
+    @IsString()
+    avatar: string;
+} 
