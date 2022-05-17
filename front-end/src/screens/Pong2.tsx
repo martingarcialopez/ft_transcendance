a975d845f6517a003dfa97928cebca25ae9d2367
Raphack <amamars201@protonmail.com>
2022-05-18 01:45:04 +0200

    move OK

--- front-end/src/screens/Pong2.tsx
+++ front-end/src/screens/Pong2.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from 'react';
-import { Direction, GameState, Position } from '../type/pongType';
+import { useRef, useState } from 'react';
+import { Direction, GameState } from '../type/pongType';
 import socketio from "socket.io-client";
 import { Button } from '@mui/material';
 import Canvas from '../components/Canvas';
@@ -30,34 +30,34 @@ export const Pong = () => {
     const onKeyDownHandler = (event: React.KeyboardEvent<HTMLDivElement>) => {
         console.log("event code = ")
         console.log(event.code)
+        console.log(Direction.UP)
+        console.log(Direction.DOWN)
         switch (event.code) {
             case 'KeyS' || 'ArrowDown':
-                if (direction !== Direction.UP) {
-                    setDirection(Direction.DOWN);
-                    socket.emit('move', id, "leftplayer", 1);
-                }
+                setDirection(Direction.DOWN);
+                socket.emit('move', id.toString(), "leftplayer", 1);
                 break;
             case 'KeyW' || 'ArrowUp':
-                if (direction !== Direction.DOWN) {
-                    setDirection(Direction.UP);
-                    socket.emit('move', id, "leftplayer", -1);
-                }
+                setDirection(Direction.UP);
+                socket.emit('move', id.toString, "leftplayer", -1);
                 break;
         }
         setId(id + 1);
         console.log("direction = ");
         console.log(direction);
+        console.log("id = ");
+        console.log(id);
     };
 
     const receive_socket_info = () => {
         socket.on('gameState', (...args) => {
             setGameState(args[0])
-            console.log(args);
-            console.log(args[0]);
-            console.log(args[0].ballPos);
-            console.log(args[0].ballPos.x);
-            console.log(args[0].ballPos.y);
-            console.log(gameState);
+            // console.log(args);
+            // console.log(args[0]);
+            // console.log(args[0].ballPos);
+            // console.log(args[0].ballPos.x);
+            // console.log(args[0].ballPos.y);
+            // console.log(gameState);
         });
     }
 
