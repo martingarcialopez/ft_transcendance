f0ab6791678a4a9deaead84e196d6affdab6f8ce
maxime-42 <mkayumba@student.42.fr>
2022-05-25 20:44:52 +0200

    implementation de MsgtoChat

--- front-end/src/components/ChatPrintMsg.tsx
+++ front-end/src/components/ChatPrintMsg.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { E_MsgToClient } from "../components/Event";
+import { E_MsgToClient, E_MsgtoChat } from "../components/Event";
 import { T_Msg } from "../type/chat";
-
 import { T_MsgHistory } from "../type/chat";
 
 type Props_ArrayMsg = {
@@ -20,22 +19,32 @@ function Print({ userId, content, sender }: T_MsgHistory, index: number) {
   );
 }
 
+/**
+ *
+ */
 function FilterRoom(room: T_Msg | any, roomId: number) {
   return room.filter((item: any) => item.roomId === roomId);
 }
 
 export function PrintMsg({ currentMsg, infoMsg }: Props_ArrayMsg) {
-  const [msgHistory, SetMsg] = useState<any>([]);
-  E_MsgToClient(SetMsg);
+  const [msgHistory, setMsg] = useState<any>([]);
+  const [msgOtherUsers, setMsgOtherUsers] = useState<T_MsgHistory[]>([]);
+  E_MsgToClient(setMsg);
   const msgHistoryFilter = FilterRoom(msgHistory, infoMsg.roomId);
   const currentMsgFilter = FilterRoom(currentMsg, infoMsg.roomId);
   /* console.log("roomId:", infoMsg.roomId);
    * console.log("msgHistoryFilter:", msgHistoryFilter);
    * console.log("currentMsgFilter:", currentMsgFilter); */
+  E_MsgtoChat(msgOtherUsers, setMsgOtherUsers);
+  const msgOtherUsersFilter = msgOtherUsers.filter(
+    (item: T_MsgHistory) => item.roomId !== infoMsg.roomId
+  );
+  //console.log("msgOtherUsers:", msgOtherUsersFilter);
   return (
     <>
       {msgHistoryFilter.map(Print)}
       {currentMsgFilter.map(Print)}
+      {msgOtherUsersFilter.map(Print)}
     </>
   );
 }
