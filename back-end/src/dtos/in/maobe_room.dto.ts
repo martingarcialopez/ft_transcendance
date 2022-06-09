fa8e894f6cd120609c481e1d1781834fe4b5f3c6
xibaochat <wmmxxbb@gmail.com>
2022-06-09 20:03:09 +0200

    add room dto

---
+++ back-end/src/dtos/in/maobe_room.dto.ts
@@ -0,0 +1,19 @@
+import { ArrayMinSize, IsArray, IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { User } from '../../models/user.entity'
+export class RoomDto {
+	@IsString()
+	@IsNotEmpty()
+	public name: string;
+
+	@IsString()
+    @IsNotEmpty()
+    public typeRoom: string;
+
+	@IsString()
+    public password: string;
+
+	@IsArray()
+	@IsNumber()
+	public users: User[];
+
+}
