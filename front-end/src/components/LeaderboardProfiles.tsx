6c22ccc618730cc0a0eb36b82b8bbe0a89d9973a
maxime-42 <mkayumba@student.42.fr>
2022-07-18 19:11:19 +0200

    add redirection to user profile from LeaderBord and get away the give up option

--- front-end/src/components/LeaderboardProfiles.tsx
+++ front-end/src/components/LeaderboardProfiles.tsx
@@ -1,4 +1,15 @@
 import "../styles/leaderboardStyles.css";
+import { useNavigate } from "react-router-dom";
+/*
+ * type Props = {
+ * 	username:string;
+ * }
+ *
+ * export function RedirectionToUserProfile({username}: Props) {
+ * 	const navigate = useNavigate();
+ * 	navigate(`/profile/${username}`)
+ * } */
+
 export const LeaderboardProfiles = ({ Leaderboard }: any) => {
   console.log("LeaderboardProfiles Leaderboard", Leaderboard);
   const Item = (data: any) => {
@@ -12,16 +23,26 @@ export const LeaderboardProfiles = ({ Leaderboard }: any) => {
     //         img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
     //         dt: "2022-02-10"
     //     }
+    const navigate = useNavigate();
 
     return (
       <>
         {data.map((value: any, index: any) => (
           <div className="flex" key={index}>
             <div className="item">
-              <img src={value.avatar} alt="" />
+              <img
+                src={value.avatar}
+                alt=""
+                onClick={() => navigate(`/profile/${value.name}`)}
+              />
 
               <div className="info">
-                <h3 className="name text-dark">{value.name}</h3>
+                <h3
+                  className="name text-dark"
+                  onClick={() => navigate(`/profile/${value.name}`)}
+                >
+                  {value.name}
+                </h3>
                 <span>{value.username}</span>
               </div>
             </div>
